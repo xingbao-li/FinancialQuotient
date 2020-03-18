5d638d49d4ebe70e326cb104fd52fd06a834887d
xingbao-li <287483590@qq.com>
2020-03-19 00:07:39 +0800

    add time

--- scripts/task/task_auto.py
+++ scripts/task/task_auto.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from task.task_filter_stock import  task_filter_stock
-
+import time
 class task_auto(object):
     def __init__(self):
         # 任务列表
@@ -13,6 +12,7 @@ class task_auto(object):
         list = []
 
         # 任务 1
+        from task.task_filter_stock import task_filter_stock
         list.append(task_filter_stock())
 
 
@@ -23,3 +23,6 @@ class task_auto(object):
         for task in self.task_list:
             if task.trigger():
                 task.run()
+
+                # 休眠 100 毫秒
+                time.sleep(0.1)
